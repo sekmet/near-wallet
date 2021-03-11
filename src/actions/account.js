03d85c406c761dafc9e7afa886d73ed633697fbe
marcinbodnardesktop <me@marcinbodnar.com>
2021-03-11 02:53:45 +0100

    Remove unused actions (#1446)

--- src/actions/account.js
+++ src/actions/account.js
@@ -508,7 +508,7 @@ export const switchAccount = (accountId) => async (dispatch, getState) => {
     dispatch(refreshAccount())
 }
 
-export const { selectAccount, refreshAccountOwner, refreshAccountExternal, refreshUrl, updateStakingAccount, updateStakingLockup, getBalance } = createActions({
+export const { selectAccount, refreshAccountOwner, refreshAccountExternal, refreshUrl, getBalance } = createActions({
     SELECT_ACCOUNT: wallet.selectAccount.bind(wallet),
     REFRESH_ACCOUNT_OWNER: [
         wallet.refreshAccount.bind(wallet),
@@ -527,19 +527,5 @@ export const { selectAccount, refreshAccountOwner, refreshAccountExternal, refre
          })
     ],
     REFRESH_URL: null,
-    UPDATE_STAKING_ACCOUNT: [
-        async (accountId) => await wallet.staking.updateStakingAccount([] , accountId),
-        (accountId) => ({
-            accountId,
-            ...showAlert({ onlyError: true })
-        })
-    ],
-    UPDATE_STAKING_LOCKUP: [
-        async (accountId) => await wallet.staking.updateStakingLockup(accountId),
-        (accountId) => ({
-            accountId,
-            ...showAlert({ onlyError: true })
-        })
-    ],
     GET_BALANCE: wallet.getBalance.bind(wallet)
 })
